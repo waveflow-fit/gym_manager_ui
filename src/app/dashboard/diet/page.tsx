58087bd5012df4f492fe3aef34ebe18bdc17d988
aniket <aniket.chanana@gmail.com>
2025-02-04 23:06:18 +0530

    Added coming soon template

--- src/app/dashboard/diet/page.tsx
+++ src/app/dashboard/diet/page.tsx
@@ -1,5 +1,23 @@
+import Box from '@mui/material/Box';
+import Image from 'next/image';
+
 const Diet = () => {
-  return <div>Diet</div>;
+  return (
+    <Box
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      height='100%'
+      width='100%'
+    >
+      <Image
+        src='/coming_soon.svg'
+        height='500'
+        width='500'
+        alt='coming soon'
+      />
+    </Box>
+  );
 };
 
 export default Diet;
--- src/app/dashboard/workout/page.tsx
+++ src/app/dashboard/workout/page.tsx
@@ -1,5 +1,23 @@
+import { Box } from '@mui/material';
+import Image from 'next/image';
+
 const Workout = () => {
-  return <div>Workout</div>;
+  return (
+    <Box
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      height='100%'
+      width='100%'
+    >
+      <Image
+        src='/coming_soon.svg'
+        height='500'
+        width='500'
+        alt='coming soon'
+      />
+    </Box>
+  );
 };
 
 export default Workout;
