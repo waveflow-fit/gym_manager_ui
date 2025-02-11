39a8095930461fc109d10d4896330514e3862e64
aniket <aniket.chanana@gmail.com>
2025-02-11 10:24:27 +0530

    updated function from function to arrow

--- src/components/Dashboard/Widgets/WidgetsSection.tsx
+++ src/components/Dashboard/Widgets/WidgetsSection.tsx
@@ -2,11 +2,11 @@ import { Box } from '@mui/material';
 
 import AddNewTraineeWidget from '@/components/Dashboard/Widgets/AddNewTraineeWidget';
 
-function WidgetsSection() {
+const WidgetsSection = () => {
   return (
     <Box width='100%' height='100%'>
       <AddNewTraineeWidget />
     </Box>
   );
-}
+};
 export default WidgetsSection;
