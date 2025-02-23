69070e8e28c13c42d973fc69172d6f2ac849309e
aniket <aniket.chanana@gmail.com>
2025-02-23 18:35:52 +0530

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,6 @@ import { ROUTE_URLS } from '@/common/appUrls';
 import { EUserRole } from '@/common/constants';
 import { getSession } from '@/components/SessionProvider/auth.utils';
 
-// Role-based access
 const protectedRoutes: Record<string, string[]> = {
   [ROUTE_URLS.trainees]: [EUserRole.TRAINER],
   [ROUTE_URLS.diet]: [EUserRole.TRAINER],
@@ -18,25 +17,29 @@ export const middleware = async (req: NextRequest) => {
   const cookieStore = await cookies();
   const token = cookieStore.get('token')?.value;
   if (!token) {
-    return NextResponse.next(); // let the app handle from dashboard/layout file
+    return NextResponse.next(); // Let the app handle it
   }
 
-  const user = await getSession(token);
+  const user = await getSession(token); // TODO: optimize this call this call is happening 2 times for because on getSession called in dashboard layout as well
+  if (!user) {
+    return NextResponse.next(); // let the app handle it
+  }
   if (!user?.role) {
     // No matter what route it goes it will always encounter user onboarding so let the app handle this as well
     return NextResponse.next();
   }
-  // for protected routes
+
+  // Role-based access control
   if (
     protectedRoutes[nextUrl.pathname] &&
     !protectedRoutes[nextUrl.pathname].includes(user.role)
   ) {
-    return NextResponse.redirect(new URL(ROUTE_URLS.dashboard, req.url)); // if the route doesn't have the right access
+    return NextResponse.redirect(new URL(ROUTE_URLS.dashboard, req.url));
   }
+
   return NextResponse.next();
 };
 
-// this has to be a static file
 export const config = {
   matcher: ['/dashboard/diet', '/dashboard/workout', '/dashboard/trainees'],
 };
