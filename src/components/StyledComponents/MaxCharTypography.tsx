6fa7391a3ebd03c7d48056e3950eb613d3c87362
aniket <aniket.chanana@gmail.com>
2025-03-08 11:08:55 +0530

    Made styled components as client

--- src/components/StyledComponents/MaxCharTypography.tsx
+++ src/components/StyledComponents/MaxCharTypography.tsx
@@ -1,14 +1,15 @@
+'use client';
 import { styled, Typography } from '@mui/material';
 
-const MaxCharTypography = styled(Typography)<{ maxchars: number }>(
-  ({ maxchars }) => {
-    return {
-      width: `${maxchars}ch`,
-      whiteSpace: 'nowrap',
-      overflow: 'hidden',
-      textOverflow: 'ellipsis',
-    };
-  }
-);
+const MaxCharTypography = styled(Typography)<{ maxchars: number }>(({
+  maxchars,
+}) => {
+  return {
+    width: `${maxchars}ch`,
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  };
+});
 
 export default MaxCharTypography;
