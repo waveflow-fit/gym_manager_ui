9736b9bacae083d0b897561ba7fec89da28d301b
aniket <aniket.chanana@gmail.com>
2025-03-12 00:00:11 +0530

    Added responsiveness

--- src/components/TemplateCreator/TemplateList.tsx
+++ src/components/TemplateCreator/TemplateList.tsx
@@ -27,7 +27,7 @@ const TemplateList = ({
         sx={{ overflowY: 'auto', height: '92%', mx: '-0.75rem', px: '0.75rem' }}
       >
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((e) => (
-          <Grid size={3} key={e}>
+          <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={e}>
             <TemplateCard />
           </Grid>
         ))}
