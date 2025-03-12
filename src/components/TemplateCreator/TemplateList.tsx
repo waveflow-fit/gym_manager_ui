a46aa857ce1df3bf8415911da0c777c1cb66afe6
aniket <aniket.chanana@gmail.com>
2025-03-12 14:14:01 +0530

    updated spacing

--- src/components/TemplateCreator/TemplateList.tsx
+++ src/components/TemplateCreator/TemplateList.tsx
@@ -23,7 +23,7 @@ const TemplateList = ({
       </HStack>
       <Grid
         container
-        spacing={2}
+        spacing={1}
         sx={{ overflowY: 'auto', height: '92%', mx: '-0.75rem', px: '0.75rem' }}
       >
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((e) => (
