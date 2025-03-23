f96657d365342f720ad3c1f00400a15463f3b17b
aniket <aniket.chanana@gmail.com>
2025-03-23 23:53:07 +0530

    specified client url

--- src/common/api.utils.ts
+++ src/common/api.utils.ts
@@ -19,8 +19,7 @@ const request = async <T = unknown, R = unknown>(
     ...(body ? { body: JSON.stringify(body) } : {}),
     credentials: 'include',
   };
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
-
+  const API_BASE_URL = 'http://localhost:3000'; // Specify client url ex: https://waveflowfit.vercel.app for production
   const response = await fetch(`${API_BASE_URL}${url}`, config);
 
   if (!response.ok) {
