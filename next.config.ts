41578bc0562baf752e59bddf4a0347e2215bcb33
aniket <aniket.chanana@gmail.com>
2025-03-23 19:24:33 +0530

    Updated next.config

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+        destination: 'http://localhost:8000/api/:path*',
       },
     ];
   },
