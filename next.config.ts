8000dab3b54b720c6384faab9c65afaacd14f6eb
aniket <aniket.chanana@gmail.com>
2025-03-23 23:11:51 +0530

    nits

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,9 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination:
-          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*` ||
-          'http://backend:8000/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_BASE_URL
+          ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`
+          : 'http://backend:8000/api/:path*',
       },
     ];
   },
