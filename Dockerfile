d3b4f40f5f3c53d12d9148299e461c15560cf890
aniket <aniket.chanana@gmail.com>
2025-03-23 10:27:36 +0530

    updated docker file

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ COPY . .
 
 RUN npm run build
 
-EXPOSE 8000
+EXPOSE 3000
 
 CMD ["npm", "start"]
--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
       },
     ];
   },
