cb156e00e86c53c31415584849a29ed35b285848
aniket <aniket.chanana@gmail.com>
2025-03-23 21:15:27 +0530

    updated server url

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: 'http://backend:8000/api/:path*',
       },
     ];
   },
